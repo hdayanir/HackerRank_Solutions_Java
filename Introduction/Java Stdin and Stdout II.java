d4e745a7e3ab653425eb0a34ade91171c8637f58
Hüseyin DAYANIR <hsyndayanir@gmail.com>
2021-02-07 20:55:50 +0300

    Summary

---
+++ Introduction/Java Stdin and Stdout II.java
@@ -0,0 +1,18 @@
+-- github.com/hdayanir
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double f = sc.nextDouble();
+        sc.nextLine();
+        String line = sc.nextLine();
+
+        System.out.println("String: " + line);
+        System.out.println("Double: " + f);
+        System.out.println("Int: " +  x);
+    }
+}
---
+++ Introduction/Welcome to Java!.java
@@ -0,0 +1,4 @@
+-- github.com/hdayanir
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
